b7288f99492143d227db544f2dba76fc5349437b
Kate <mika22pj@student.ju.se>
2023-01-28 17:02:11 +0100

    First commit
    Game: ufo, 1  obstacle
    Flowing lines: Trying to make them flow

---
+++ flowingLines.js
@@ -0,0 +1,70 @@
+// upper lines
+
+// pink
+function upperLines1(x, y) {
+  //upper lines to the right
+  strokeWeight(16);
+  stroke(255, 0, 127);
+  line(x + 0, y + 650, x + 50, y + 650);
+}
+
+// green
+function upperLines2(a, y) {
+  stroke(171, 255, 79);
+  line(a + 190, y + 650, a + 330, y + 650);
+}
+
+// blue
+function upperLines3(b, y) {
+  stroke(0, 79, 255);
+  line(b + 500, y + 650, b + 800, y + 650);
+}
+
+// function upperLines4(c, y) {
+//   stroke(255, 0, 127);
+//   line(x + 960, y + 650, x + 1050, y + 650);
+// }
+
+function lowerLines(x, y) {
+  stroke(131, 56, 236);
+  line(x + 40, y + 700, x + 180, y + 700);
+  stroke(255, 0, 127);
+  line(x + 460, y + 700, x + 660, y + 700);
+  stroke(171, 255, 79);
+  line(x + 870, y + 700, x + 920, y + 700);
+}
+
+// Animating obstacles
+// Obstacle number 2, upper line
+
+let x = 0;
+let a = 0;
+let b = 0;
+let c = 0;
+
+function draw() {
+  clear();
+  upperLines3(b, 0);
+  b = b + 6;
+
+  upperLines2(a, 0);
+  a = a + 6;
+
+  upperLines1(x, 0);
+  x = x + 6;
+
+  if (b > 600) {
+    b = -1300;
+  }
+
+  if (a > 600) {
+    a = -500;
+  }
+
+  if (x > 600) {
+    x = 0;
+  }
+
+  //   lowerLines(a, 0);
+  //   a = a - 2;
+}
