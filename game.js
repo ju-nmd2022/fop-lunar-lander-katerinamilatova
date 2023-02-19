957eca4c28d4d9bd25771d3d0c72d530c1491831
Kate <mika22pj@student.ju.se>
2023-02-19 19:35:29 +0100

    Draw Obstacle at one place

--- game.js
+++ game.js
@@ -1,33 +1,36 @@
 import { Cow } from "cow.js";
-import { ObstacleOne } from "obstacleOne.js";
-import { FlowingLine } from "./flowingLines";
 import { Ufo } from "./ufo";
 import { CowHolder } from "./earnCowAnimation";
 import { ExtendLinesRight } from "./extendLinesRight";
 import { ExtendLinesLeft } from "./extendLinesLeft";
+import { ObstacleOne } from "./obstacleOne";
 
 export class Game {
   // in constructor can be only the things that will change - not ufo (ufo will be in every game the same)
   constructor(playerName) {
-    this.playerName = playerName;
-    this.ufo = new Ufo(0.8);
-    this.cowList = [];
-    this.obstacleOneList = [];
-    // this.flowingLinesList = [];
-    this.cowHolderList = [];
-    this.extendLine = new ExtendLinesRight(100, 600, "#004FFF");
-    this.xD = new ExtendLinesLeft(500, 600, "#004FFF");
-
     // set up constants
     this.ufoY = 0;
     this.velocity = 8;
     this.acceleration = 0.2;
     this.obstacleVelocity = 2;
 
+    this.playerName = playerName;
+    this.ufo = new Ufo(0.8);
+    this.cowList = [];
+    this.cowHolderList = [];
+    this.obstacleList = [];
+    this.setUp();
+
     this.addCow();
-    this.addObstacleOne();
-    // this.addFlowingLine();
     this.addCowHolder();
+  }
+
+  setUp() {
+    let extendLineRight = new ExtendLinesRight(100, 600, "#004FFF");
+    let extendLineLeft = new ExtendLinesLeft(500, 600, "#004FFF");
+    let obstacleOne = new ObstacleOne(200, 800);
+
+    this.obstacleList.push(extendLineRight, extendLineLeft, obstacleOne);
   }
 
   /*
@@ -35,27 +38,27 @@ export class Game {
     of the draw function in run.js to return from the function and end the game
   */
   run() {
-    // draw ufo and move it
-
-    this.ufo.Draw(this.ufoY);
-
-    this.ufoY = this.ufoY + this.velocity;
+    //ocitovat - myslím že to je od Garrita
     this.velocity = this.velocity + this.acceleration;
     if (mouseIsPressed) {
       this.velocity = this.velocity - 0.8;
       this.ufo.FlyUp();
     }
 
-    //move obstacles up
-    this.extendLine.Draw(this.obstacleVelocity);
-    this.xD.Draw(this.obstacleVelocity);
+    // draw ufo and move it
+    this.ufo.Draw(this.ufoY);
+    this.ufoY = this.ufoY + this.velocity;
 
-    // for (let i = 0; i < this.nejakePole.length; i++) {
-    //   // OCITOVAT
-    //   if (typeof someObject.Draw == "function") {
-    //     this.nejakePole[i].Draw();
-    //   }
-    // }
+    for (let i = 0; i < this.obstacleList.length; i++) {
+      /* OCITOVAT
+      this is just safety check that the object has a defined method draw
+      I am going through all children in a class and I am telling them to do HW 
+      which is specified in the work book (everyone knows what to do - this is the method Draw) - 
+      this is how I am telling all my objects to be drawn */
+      if (typeof this.obstacleList[i].Draw == "function") {
+        this.obstacleList[i].Draw(this.obstacleVelocity);
+      }
+    }
 
     for (let i = 0; i < this.cowList.length; i++) {
       this.cowList[i].draw();
@@ -64,14 +67,6 @@ export class Game {
     for (let i = 0; i < this.cowHolderList.length; i++) {
       this.cowHolderList[i].draw();
     }
-
-    for (let i = 0; i < this.obstacleOneList.length; i++) {
-      this.obstacleOneList[i].Draw(this.obstacleVelocity);
-    }
-
-    // for (let i = 0; i < this.flowingLinesList.length; i++) {
-    //   this.flowingLinesList[i].draw();
-    // }
 
     // contidions to end the game
     if (this.ufoY <= -100 || this.ufoY >= 700) {
@@ -93,15 +88,4 @@ export class Game {
     let newCowHolder = new CowHolder(200, 200, 200, 200);
     this.cowHolderList.push(newCowHolder);
   }
-
-  addObstacleOne() {
-    print("Creating obstacle");
-    let newObstacleOne = new ObstacleOne(200, 400);
-    this.obstacleOneList.push(newObstacleOne);
-  }
-
-  // addFlowingLine() {
-  //   let newFlowingLine = new FlowingLine();
-  //   this.flowingLinesList.push(newFlowingLine);
-  // }
 }
